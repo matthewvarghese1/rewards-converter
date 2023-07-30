0c339c7f739f705fa7738649e2988fb5d419b68f
matthewvarghese1 <mtv5115@psu.edu>
2023-07-30 14:40:22 -0400

    final

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,17 @@
 public class RewardValue {
+    public double cashValue;
+    public double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035; // Convert cash to miles
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
 }
